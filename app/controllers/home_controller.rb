8c1a35eab645af8f0eb538776258d8f9cc5c985f
guregure13 <guregure13@gmail.com>
2016-12-19 16:30:28 +0000

    시크릿 코드

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,10 @@ class HomeController < ApplicationController
     
     
   end
+  
+  def sendemail
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/item' => "home#item"
   get '/media' => "home#media"
   get '/contact' => "home#contact"
+  
+  post '/sendemail' =>"home#sendemail"
 
   root 'home#index' 
 
